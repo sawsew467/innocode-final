90dc71a3be89a733c58f324960d1bd391f3f9693
sawsew467 <thangtvb.dev@gmail.com>
2024-07-11 14:18:47 +0700

    feat: chat bot styled

---
+++ src/components/modules/Chat/ChatBody.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ChatBody() {
+  return <div className="flex-1 bg-green-100/20">ChatBody</div>;
+}
+
+export default ChatBody;
--- src/components/modules/Chat/index.tsx
+++ src/components/modules/Chat/index.tsx
@@ -8,10 +8,15 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import Image from "next/image";
+import logo from "@public/images/logo-white.png";
+import ChatHeader from "./ChatHeader";
+import ChatBody from "./ChatBody";
+import ChatFooter from "./ChatFooter";
 
 function Chat() {
   return (
-    <div className="fixed bottom-10 right-20 z-10">
+    <div className="fixed bottom-10 right-20 z-50">
       <Dialog>
         <DialogTrigger asChild>
           <div className="hover:bg-primary-dark cursor-pointer rounded-full bg-primary px-4 py-4 transition-all">
@@ -22,16 +27,24 @@ function Chat() {
         </DialogTrigger>
         <DialogContent
           close={false}
-          className="min-h-[400px] max-w-4xl bg-white/40 backdrop-blur-lg"
+          className="min-h-[600px] max-w-4xl bg-white/20 outline-none backdrop-blur-xl"
         >
           <div className="flex gap-5">
             <div className="flex flex-1 flex-col">
-              <div className="flex justify-between">
-                <h5 className="font-title text-3xl text-white">Trợ lý ảo</h5>
-                <span>Verson 1.0</span>
+              <div className="mt-5 flex items-end justify-between">
+                <h5 className="font-title text-2xl text-white">Trợ lý ảo</h5>
+                <span className="text-white/70">Verson 1.0</span>
+              </div>
+              <div className="flex-1"></div>
+              <div>
+                <Image src={logo} alt="" height={52} />
               </div>
             </div>
-            <div className="flex-[2]">ahihi</div>
+            <div className="flex flex-[2] flex-col rounded-md bg-white">
+              <ChatHeader />
+              <ChatBody />
+              <ChatFooter />
+            </div>
           </div>
         </DialogContent>
       </Dialog>
