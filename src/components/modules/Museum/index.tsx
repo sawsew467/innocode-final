53f08b453846166a4f487ee6a790a8b5fbea7a73
Lê Đức Anh Phương <leducanhphuongdev@gmail.com>
2024-07-11 14:01:05 +0700

    done model cho trien lam

--- src/components/modules/Museum/index.tsx
+++ src/components/modules/Museum/index.tsx
@@ -1,12 +1,104 @@
 "use client";
 
 import { useState } from "react";
-import Experience from "./Experience";
+import Image, { StaticImageData } from "next/image";
+import { AnimatePresence, motion } from "framer-motion";
 
+import Experience from "./Experience";
+import { X } from "lucide-react";
+
+import background from "@public/images/background_frames.png";
+import fpt from "@public/timeline/thpt_fpt_3.jpg";
+
+type InterfaceContent = {
+  title: string;
+  image: StaticImageData;
+  descrision: string;
+};
 function MuseumModule() {
-  const [isShowDrawer, setIsShowDrawer] = useState(false);
+  const [isShowDrawer, setIsShowDrawer] = useState(true);
+  const content: InterfaceContent = {
+    title: "Hoa ngu vai l",
+    image: fpt,
+    descrision:
+      "eheheheeheheheeheheheeheheheeheheheeheheheeheheheeheheheeheheheeheheheeheheheeheheheeheheheeheheheeheheheeheheheeheheheeheheheeheheheehehehe",
+  };
+
+  const handleHiddenModel = () => {
+    setIsShowDrawer(false);
+  };
   return (
-    <section className="h-screen w-screen">
+    <section className="relative h-screen w-screen">
+      <AnimatePresence mode="wait">
+        {isShowDrawer && (
+          <div className="absolute left-10 top-1/2 z-20 max-w-[40%] -translate-y-1/2">
+            <motion.div
+              initial={{
+                x: -100,
+                y: 100,
+                opacity: 0,
+                scale: 0.5,
+              }}
+              animate={{
+                x: 0,
+                y: 0,
+                opacity: 1,
+                scale: 1,
+              }}
+              transition={{
+                type: "spring",
+                duration: 0.75,
+              }}
+              exit={{
+                x: -100,
+                y: 100,
+                opacity: 0,
+                scale: 0.5,
+              }}
+              className="flex w-full flex-col gap-5 rounded-xl bg-white/60 bg-cover bg-center py-10 drop-shadow-2xl backdrop-blur-lg"
+            >
+              <span className="absolute left-0 right-0 top-0 h-10 z-30">
+                <button
+                  onClick={handleHiddenModel}
+                  className="absolute right-1 top-1 rounded-full p-2 transition-all duration-500 hover:rotate-180"
+                >
+                  <X />
+                </button>
+              </span>
+              <div className="relative rounded-2xl">
+                <span className="group w-full overflow-hidden">
+                  <span
+                    style={{
+                      maskImage: "url('/images/background_frames.png')",
+                      maskRepeat: "no-repeat",
+                      maskPosition: "center",
+                      maskSize: "contain",
+                    }}
+                  >
+                    <Image
+                      src={content.image}
+                      alt={content.title}
+                      width={600}
+                      height={400}
+                      className="bg-background_library transition-all duration-500 group-hover:scale-125"
+                    />
+                  </span>
+                </span>
+              </div>
+              <div className="text-wrap break-words px-12">
+                <h2 className="font-title text-3xl font-medium text-black">
+                  {content.title}
+                </h2>
+              </div>
+              <div className="w-full text-wrap break-words px-12">
+                <p className="w-full font-title text-lg font-normal">
+                  {content.descrision}
+                </p>
+              </div>
+            </motion.div>
+          </div>
+        )}
+      </AnimatePresence>
       <Experience setIsShowDrawer={setIsShowDrawer} />
     </section>
   );
